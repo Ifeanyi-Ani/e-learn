32e95822b89250690632a9eaa5c80d0abe555975
Ifeanyi-Ani <ifeanyiani155@gmail.com>
2023-10-17 08:40:15 +0100

    updated

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,21 @@
-import express, { urlencoded } from "express";
+import express, { NextFunction, Response, Request } from "express";
+require('dotenv').config();
 export const app= express();
 import cors from "cors";
 import cookieParser from 'cookie-parser';
 
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({limit:"100mb"}));
 app.use(express.urlencoded({extended:true}));
 
 app.use(cookieParser());
-app.use(cors())
+// app.use(cors({
+//     origin: process.env.ORIGIN
+// }))
+app.use(cors());
+
+
+app.all("*", (req:Request,res:Response,next:NextFunction)=>{
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode=404;
+    next(err);
+})
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,12 @@
 import {app} from "./app";
-require('dotenv').config();
+import mongoose from 'mongoose';
+
+// mongoose.connect(process.env.CON_STR,{
+//     useUnifiedTopology:true,
+//     useNewUrlParser:true,
+// }).then(()=>console.log("server is connected")).catch((err)=>console.log(err.message))
+
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`server is running on ${process.env.PORT}`)
+    console.log(`server is running on port ${process.env.PORT}`)
 })
