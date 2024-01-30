ad633786aa1c20e1a12c9c03111b3199d7d80b31
Ifeanyi-Ani <ifeanyiani155@gmail.com>
2024-01-30 14:55:37 +0100

    update: set redis up

--- server/dist/src/utils/redis.js
+++ server/dist/src/utils/redis.js
@@ -1,2 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.redis = void 0;
+const ioredis_1 = require("ioredis");
+require("dotenv").config();
+const redisClient = () => {
+    if (process.env.REDIS_URL) {
+        console.log("Redis Connected");
+        return process.env.REDIS_URL;
+    }
+    else {
+        throw new Error("Redis connection failed");
+    }
+};
+exports.redis = new ioredis_1.Redis(redisClient());
